795572fef09efb7d17175ea92097c32b33f17127
flaviodeangelis56 <deangelisflavio@gmail.com>
2023-10-09 14:56:43 +0200

    complete-exercise1

---
+++ Exercise1.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        int[] es1 = new int[5];
+        Random random = new Random();
+
+        for (int i = 0; i < es1.length; i++) {
+            int numeroCasuale = random.nextInt(10) + 1;
+            es1[i] = numeroCasuale;
+        }
+
+
+        for (int i = 0; i < es1.length; i++) {
+            System.out.println(es1[i] + " ");
+        }
+
+
+        while (true) {
+
+            Scanner input = new Scanner(System.in);
+            System.out.println("inserisci un numero da 1 a 10 da inserire nell'array, inserisci 0 per interrompere il programma");
+            int newNumber = Integer.parseInt(input.nextLine());
+            if (newNumber == 0) {
+                break;
+            }
+            try {
+                System.out.println("inserisci il numero della posizione che vuoi modificare");
+                int positionNumber = Integer.parseInt(input.nextLine());
+
+                es1[positionNumber] = newNumber;
+
+                
+                for (int i = 0; i < es1.length; i++) {
+                    System.out.println(es1[i] + " ");
+                }
+            } catch (ArrayIndexOutOfBoundsException ex) {
+                System.out.println("index dell'array non valido");
+            } catch (Exception ex) {
+                System.out.println("errore generico");
+            }
+        }
+
+
+    }
+}
